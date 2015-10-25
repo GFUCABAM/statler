3085cc00c41538b1b7ccf0e558146fbf41a88d87
Ben Delaney <bdelaney13@georgefox.edu>
2015-10-24 17:13:01 -0700

    An epiphany!

--- statler/settings.py
+++ statler/settings.py
@@ -82,6 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'statler',
         'USER': 'postgres',
+        # EPIPHANY: Let's reference an environment variable here. (Maybe for the username, too)
         'PASSWORD': '12345', # It's super secure!
         'HOST': '127.0.0.1',
         'PORT': '5432',
