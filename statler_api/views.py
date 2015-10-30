7fd91272c5f29ff59b957e7765f7e04be07e6e8b
Charles Haneberg <chaneberg13@georgefox.edu>
2015-10-29 23:40:36 -0700

    Added play/ to URLs. Updated views.py files to use the PlayDAO object. Made webpage resize better.

--- statler_api/views.py
+++ statler_api/views.py
@@ -14,13 +14,13 @@ def getPlayList(request):
     """called when a GET request is sent to /api/play-list/
     returns json of the list of plays
     see https://docs.djangoproject.com/en/1.8/topics/serialization/"""
-    playList = Play.objects.all()
+    playList = PlayDAO.objects.all()
     #TODO: wrap playList in another object to control what fields are sent
     return HttpResponse(serializers.serialize("json", playList))
 
 def getPlayDetail(request, play_id):
     """called when a GET request is sent to /api/play/<play_id>
     returns json for the play"""
-    play = get_object_or_404(Play, url_title=play_id)
+    play = get_object_or_404(PlayDAO, url_title=play_id)
     #TODO: wrap play in another object to control what fields are sent
     return HttpResponse(serializers.serialize("json", [play]))
