043069b4f653e63d028120ed86aa293123214a8f
Charles Haneberg <chaneberg13@georgefox.edu>
2015-11-05 17:24:53 -0800

    reviews now display on site

--- statler_api/views.py
+++ statler_api/views.py
@@ -20,6 +20,12 @@ def getPlayList(request):
     #TODO: get an actual PlayListDAO object instead of all plays
 
     aPlayDictList = [vars(Play(x)) for x in PlayDAO.objects.all()]
+
+    # Remove reviews from each play dict, because we aren't going
+    # that deep in the object graph
+    for aPlayDict in aPlayDictList:
+        del aPlayDict["reviews"]
+        
     return HttpResponse(json.dumps(aPlayDictList))
     
 
@@ -28,4 +34,12 @@ def getPlayDetail(request, play_id):
     returns json for the play"""
     
     play = vars(Play(get_object_or_404(PlayDAO, url_title=play_id)))
+
+    # Convert reviews to dicts so they will serialize
+    reviews = play["reviews"]
+    for i in range(len(reviews)):
+        reviews[i] = vars(reviews[i])
+        # convert timestamps to iso strings so they will serialize
+        reviews[i]["timestamp"] = reviews[i]["timestamp"].isoformat()
+    
     return HttpResponse(json.dumps(play))
